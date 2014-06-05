4c39b7b8d6c4253d2790db17b5b4c042bcbfa2d1
thomas-elite <thomas@elitedesign.com.tw>
2014-06-05 18:14:10 +0800

    加一減一範例

--- src/com/thenewboston/travis/StartingPoint.java
+++ src/com/thenewboston/travis/StartingPoint.java
@@ -9,14 +9,44 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 import android.os.Build;
 
 public class StartingPoint extends ActionBarActivity {
 
+	int counter;
+	Button add;
+	Button sub;
+	TextView display;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starting_point);
+        
+        counter=0;
+        add = (Button)findViewById(R.id.bAdd);
+        sub = (Button)findViewById(R.id.bSubtract);
+        display = (TextView)findViewById(R.id.tvDisplay);
+        add.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				counter += 1;
+				display.setText("Your total is " + counter);
+			}
+		});
+    	sub.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				counter -= 1;
+				display.setText("Your total is " + counter);
+			}
+		});
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
