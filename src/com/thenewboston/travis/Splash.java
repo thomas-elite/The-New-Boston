7f2e9750eb1af8bcce69d12133193a690a9281c2
thomas-elite <thomas@elitedesign.com.tw>
2014-06-09 11:22:53 +0800

    lesson 17/200, splash sound

--- src/com/thenewboston/travis/Splash.java
+++ src/com/thenewboston/travis/Splash.java
@@ -2,15 +2,20 @@ package com.thenewboston.travis;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class Splash extends Activity {
 
+	MediaPlayer ourSong;
+	
 	@Override
 	protected void onCreate(Bundle TravisLoveBacon) {
 		// TODO Auto-generated method stub
 		super.onCreate(TravisLoveBacon);
 		setContentView(R.layout.splash);
+		ourSong = MediaPlayer.create(Splash.this, R.raw.splashsound);
+		ourSong.start();
 		Thread timer = new Thread(){
 			public void run(){
 				try{
@@ -24,6 +29,14 @@ public class Splash extends Activity {
 			}
 		};
 		timer.start();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		ourSong.release();
+		finish();
 	}
 
 }
